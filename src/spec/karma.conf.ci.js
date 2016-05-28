8cbbd1ef19abcdb318aa68f841ab65367691ae45
Hamlet Hakobyan <hamlethakobyan@workfront.com>
2016-05-29 00:06:56 +0400

    Configured custom build step.

---
+++ src/spec/karma.conf.ci.js
@@ -0,0 +1,15 @@
+/* eslint-env node */
+
+// Karma configuration
+// Generated on Mon May 23 2016 14:05:11 GMT+0400 (Caucasus Standard Time)
+'use strict';
+
+module.exports = function(config) {
+    var configFn = require('./karma.conf.js');
+    configFn(config);
+    config.set({
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: false
+    });
+};
